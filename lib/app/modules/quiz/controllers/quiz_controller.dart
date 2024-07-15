01b8054e64622a4892735d0ea3d7225bcc22979b
adyfp24 <adyfp24@gmail.com>
2024-07-15 09:44:25 +0000

    feat: BE integration for get all quiz

--- lib/app/modules/quiz/controllers/quiz_controller.dart
+++ lib/app/modules/quiz/controllers/quiz_controller.dart
@@ -3,7 +3,30 @@ import 'package:get/get.dart';
 import 'package:safeloan/app/modules/quiz/models/quiz_model.dart';
 
 class QuizController extends GetxController {
-  
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   var quizList = <Quiz>[].obs;
+  var questionList = <Question>[].obs;
+
+  Stream<QuerySnapshot> getQuizList() {
+    return firestore.collection('quiz').snapshots();
+  }
+
+  void updateQuizList(QuerySnapshot snapshot) {
+    quizList.clear();
+    quizList.addAll(snapshot.docs.map((doc) => Quiz.fromDocument(doc)).toList());
+  }
+
+  Stream<QuerySnapshot> getQuestionList(String quizId) {
+    return firestore
+        .collection('quiz')
+        .doc(quizId)
+        .collection('question')
+        .snapshots();
+  }
+
+  void updateQuestionList(QuerySnapshot snapshot) {
+    questionList.clear();
+    questionList.addAll(snapshot.docs.map((doc) => Question.fromDocument(doc)).toList());
+  }
 }
