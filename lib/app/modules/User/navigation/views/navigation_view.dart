bfcc21299ece0f83f9ab23cc46c8815a88b30d2d
lyeiva86 <ulul10039@gmail.com>
2024-07-21 08:57:28 +0700

    feat: styling navbar in user

--- lib/app/modules/User/navigation/views/navigation_view.dart
+++ lib/app/modules/User/navigation/views/navigation_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:safeloan/app/modules/User/tab_quiz/views/tab_quiz_view.dart';
+import 'package:safeloan/app/utils/AppColors.dart';
 
 import '../../finance/views/finance_view.dart';
 import '../../homepage/views/homepage_view.dart';
@@ -8,45 +9,79 @@ import '../../profile/views/profile_view.dart';
 import '../controllers/navigation_controller.dart';
 
 class NavigationView extends GetView<NavigationController> {
-  const NavigationView({Key? key}) : super(key: key);
+  const NavigationView({super.key});
 
   @override
   Widget build(BuildContext context) {
     final NavigationController controller = Get.put(NavigationController());
 
-    final List<Widget> _pages = [
+    final List<Widget> pages = [
       HomepageView(),
       const FinanceView(),
       const TabQuizView(),
       const ProfileView(),
     ];
 
-    return Obx(() => Scaffold(
-          body: _pages[controller.selectedIndex.value],
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: controller.selectedIndex.value,
-            onTap: controller.changePage,
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.grey,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.attach_money),
-                label: 'Finance',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.quiz),
-                label: 'Quiz',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profile',
+    return Scaffold(
+      body: Obx(() => pages[controller.selectedIndex.value]),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: AppColors.primaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Obx(() => Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildNavigationBarItem(Icons.home, 'Home', 0, controller),
+              _buildNavigationBarItem(Icons.account_balance_wallet, 'Finance', 1, controller),
+              _buildNavigationBarItem(Icons.quiz, 'Quiz', 2, controller),
+              _buildNavigationBarItem(Icons.person, 'Profile', 3, controller),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavigationBarItem(IconData icon, String label, int index, NavigationController controller) {
+    final isSelected = controller.selectedIndex.value == index;
+    return GestureDetector(
+      onTap: () => controller.changePage(index),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        decoration: isSelected ? BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ) : null,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? AppColors.primaryColor : Colors.white,
+              size: 24,
+            ),
+            if (isSelected) ...[
+              const SizedBox(width: 8),
+              Text(
+                label,
+                style: const TextStyle(
+                  color: AppColors.primaryColor,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
-          ),
-        ));
+          ],
+        ),
+      ),
+    );
   }
 }
