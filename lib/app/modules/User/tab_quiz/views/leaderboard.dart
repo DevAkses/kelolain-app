7e0edd42c6d9481989bfe5985a9194035a92a63d
adyfp24 <adyfp24@gmail.com>
2024-07-23 16:20:27 +0000

    fix: notification ui

--- lib/app/modules/User/tab_quiz/views/leaderboard.dart
+++ lib/app/modules/User/tab_quiz/views/leaderboard.dart
@@ -4,30 +4,69 @@ import 'package:safeloan/app/utils/warna.dart';
 class LeaderBoard extends StatelessWidget {
   const LeaderBoard({super.key});
 
+  Widget cardHeader(
+       String deskripsi, String linkGambar, VoidCallback onTap) {
+    return Container(
+      width: double.infinity,
+      height: 150,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.05),
+            spreadRadius: 0,
+            blurRadius: 30,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Center(
+        child: ListTile(
+          subtitle: Text(
+            deskripsi,            
+          ),
+          trailing: SizedBox(
+              width: 100,
+              height: 100,
+            child: Image.asset(
+              linkGambar,
+              fit: BoxFit.cover,
+            ),
+          ),
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Dummy data
     final leaderboardData = [
       {'name': 'Alice', 'points': 1500},
       {'name': 'Bob', 'points': 1200},
       {'name': 'Charlie', 'points': 1100},
       {'name': 'David', 'points': 1000},
       {'name': 'Eve', 'points': 950},
-      {'name': 'Dev', 'points': 900},
+      {'name': 'Anda', 'points': 900},
       {'name': 'Ulul', 'points': 930},
       {'name': 'Yusril', 'points': 800},
     ];
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Leaderboard', style: Utils.header,),
+        title: const Text(
+          'Leaderboard',
+          style: Utils.header,
+        ),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Top 3 Leaderboard
+            cardHeader('Ayo selesaikan kuis dan tantangan untuk mengumpulkan poin dan menjadi nomor satu!', 'assets/images/trophy.png', (){}),
             Container(
               height: 200,
               child: ListView.builder(
@@ -43,11 +82,13 @@ class LeaderBoard extends StatelessWidget {
                       children: [
                         CircleAvatar(
                           radius: 30,
-                          backgroundColor: Utils.biruEmpat,
+                          backgroundColor: Colors.blue[50],
                           child: Text(
                             '${index + 1}',
                             style: const TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold, color: Utils.biruSatu),
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Utils.biruSatu),
                           ),
                         ),
                         const SizedBox(height: 8),
@@ -79,7 +120,6 @@ class LeaderBoard extends StatelessWidget {
                 },
               ),
             ),
-            const SizedBox(height: 10),
             // Remaining Leaderboard
             Expanded(
               child: Padding(
@@ -90,11 +130,24 @@ class LeaderBoard extends StatelessWidget {
                     final user = leaderboardData[index + 3];
                     return ListTile(
                       leading: Text('${index + 4}'),
-                      title: Text('${user['name']!}'),
+                      title: Text(
+                        '${user['name']}',
+                        style: TextStyle(
+                          fontWeight: user['name'] == 'Anda'
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color: user['name'] == 'Anda'
+                              ? Utils.biruDua
+                              : Colors.black,
+                        ),
+                      ),
                       trailing: Row(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          Text('${user['points']}', style: const TextStyle(color: Colors.grey),),
+                          Text(
+                            '${user['points']}',
+                            style: const TextStyle(color: Colors.grey),
+                          ),
                           const SizedBox(width: 8),
                           Container(
                             padding: const EdgeInsets.all(1.0),
